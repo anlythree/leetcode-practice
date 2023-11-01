3d12588992f342324de781b57a173294fa924ffd
anlythree <15610462126@163.com>
2023-11-01 10:15:05 +0800

    code sync

--- src/main/java/com/anlythree/Main.java
+++ src/main/java/com/anlythree/Main.java
@@ -1,15 +1,28 @@
 package com.anlythree;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List list = new ArrayList();
-        list.add("haha");
-        list.add("haha");
-        list.add("haha");
-        System.out.println(list);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if( i == 0){
+                    break;
+                }
+                System.out.println("父循环第:"+i+"次"+"子循环第:"+j+"次");
+            }
+            System.out.println("父循环第:"+i+"次");
+        }
+    }
+
+    public static List page(List list,int pageNum,int pageSize){
+        return null;
+
     }
 
     public void fun1(){
@@ -21,5 +34,6 @@ public class Main {
         String intern = a.intern();
         System.out.println(intern);
     }
+
 
 }
